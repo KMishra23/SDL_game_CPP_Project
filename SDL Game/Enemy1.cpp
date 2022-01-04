34c97a49356a0cae40925ea8cfa1e6cfc3c61509
jainavsanghvi10 <79016037+jainavsanghvi10@users.noreply.github.com>
2022-01-04 10:56:24 +0530

    Delete Enemy1.cpp

--- SDL Game/Enemy1.cpp
+++
@@ -1,7 +0,0 @@
-#include "Enemy1.h"
-#include "Game.h"
-
-Enemy1 :: Enemy1(const char* textureSheet, int x, int y, int scale, int mapNum) : GameObject(textureSheet, x, y, scale)
-{
-	this->mapNumber = mapNum;
-}
